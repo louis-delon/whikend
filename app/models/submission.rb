93b65ec6beddee8f1e105fca33f1a4e14621252b
louis delon <louleman34@gmail.com>
2018-02-26 16:34:10 +0100

    creating seed

---
+++ app/models/submission.rb
@@ -0,0 +1,7 @@
+class Submission < ApplicationRecord
+  belongs_to :user
+  belongs_to :trip
+  validates :content, presence: true
+  validates :trip, presence: true
+  validates :user, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Submission.destroy_all
+Message.destroy_all
+Review.destroy_all
+Trip.destroy_all
+Hike.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name
+  )
+end
+
+puts "Creating hikes..."
+
+10.times do
+  Hike.create!(
+    duration: [1..10].sample
+    location: Faker::Address.city,
+  )
+end
+
+puts "Creating trips..."
+10.times do
+  Trip.create!(
+    title: Faker::Hipster.sentence,
+    description: Faker::Hipster.paragraph,
+    location: Faker::Address.city,
+    hike_id: ((Hike.first.id)..(Hike.last.id)).to_a.sample,
+    user_id: ((User.first.id)..(User.last.id)).to_a.sample,
+    date: Date.today
+  )
+end
+
+puts "Creating reviews..."
+
+10.times do
+  Review.create!(
+    content: Faker::Hipster.paragraph,
+    rating: [1..5].sample
+    sender_id: ((User.first.id)..(User.last.id)).to_a.sample,
+    receiver_id: ((User.first.id)..(User.last.id)).to_a.sample,
+    trip_id: ((Trip.first.id)..(Trip.last.id)).to_a.sample
+  )
+end
+
+puts "Creating messages..."
+
+100.times do
+  Message.create!(
+    content: Faker::Hipster.paragraph,
+    user_id: ((User.first.id)..(User.last.id)).to_a.sample,
+    trip_id: ((Trip.first.id)..(Trip.last.id)).to_a.sample
+  )
+end
+
+puts "Creating submissions..."
+
+66.times do
+  Submission.create!(
+    content: Faker::Hipster.paragraph,
+    user_id: ((User.first.id)..(User.last.id)).to_a.sample,
+    trip_id: ((Trip.first.id)..(Trip.last.id)).to_a.sample
+    accepted: true
+  )
+end
+
+34.times do
+  Submission.create!(
+    content: Faker::Hipster.paragraph,
+    user_id: ((User.first.id)..(User.last.id)).to_a.sample,
+    trip_id: ((Trip.first.id)..(Trip.last.id)).to_a.sample
+    accepted: false
+  )
+end
+
+
+
