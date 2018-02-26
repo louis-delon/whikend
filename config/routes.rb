6d272f5dbe6af86add84c15138aa3021646dd369
louis delon <louleman34@gmail.com>
2018-02-26 17:11:59 +0100

    making routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
+  resources :users, only: [:show, :edit, :update]
+
+  resources :trips do
+    resources :submissions, except: [:update, :edit, :show]
+    resources :messages, only: [:create]
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
