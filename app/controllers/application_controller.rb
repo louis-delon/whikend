8e6e5abba953b22bd07e80006391e82a43474824
alexandrebk <alexandre.b2506@gmail.com>
2018-03-01 15:03:00 +0100

    avatar cloudinary v2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,10 +24,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :description, :avatar_url])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar_url])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :description])
   end
 
 end
