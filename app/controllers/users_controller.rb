127ac961cf4d3c7fee42cb27da9d7a9bac5b2576
louis delon <louleman34@gmail.com>
2018-03-02 17:58:15 +0100

    update the profile and display user reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
 before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
-
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   private
@@ -18,6 +18,10 @@ before_action :set_user, only: [:show, :edit, :update]
   def set_user
     @user = User.find(params[:id])
     authorize(@user)
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :age, :email)
   end
 
 end
