eb8c427f2c6fa17f09a4408dc7d9c748a891e852
alexandrebk <alexandre.b2506@gmail.com>
2018-03-05 15:06:58 +0100

    emailer confirmation for approve

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,25 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user
 
-    mail to: @user.email, subject: "Validation Rando"
+    mail to: @user.email, subject: "Welcome to Whikend"
   end
+
+  def create_hike(user)
+    @user = user
+
+    mail to: @user.email, subject: "Nouvelle Rando"
+  end
+
+  def select(user)
+    @user = user
+
+    mail to: @user.email, subject: "Participation Rando"
+  end
+
+  def reject(user)
+    @user = user
+
+    mail to: @user.email, subject: "Refus Rando"
+  end
+
 end
