5607475ffc8e44cece0a48aca93bcd5bec49c55f
alexandrebk <alexandre.b2506@gmail.com>
2018-03-07 10:09:26 +0100

    changement de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,8 @@ francois = User.create!(
   password: 'aaaaaa',
   first_name: 'Francois',
   last_name: 'Mitterand',
-  description: 'Rejoignez moi pour une petit rando Monsieur le Premier Ministre',
-  age: 103,
+  description: 'Résistant, Homme de lettre passionné par les livres',
+  age: 101,
   remote_avatar_url: 'https://cache.20minutes.fr/photos/2014/12/31/president-francois-mitterrand-lors-9633-diaporama.jpg'
   )
 
@@ -28,7 +28,7 @@ roger = User.create!(
   first_name: 'Roger',
   last_name: 'Hanin',
   description: 'Je suis le beauf de Tonton',
-  age: 87,
+  age: 93,
   remote_avatar_url: 'http://md1.libe.com/photo/715188-france-bio-hanin.jpg?modified_at=1423671399&width=960'
   )
 
@@ -38,7 +38,7 @@ jack = User.create!(
   first_name: 'Jack',
   last_name: 'Lang',
   description: 'Vive la musique en rando!',
-  age: 87,
+  age: 78,
   remote_avatar_url: 'https://www.ina.fr/images_v2/620x349/CPC88007257.jpeg'
   )
 
@@ -47,7 +47,7 @@ jacques = User.create!(
   password: 'aaaaaa',
   first_name: 'Jacques',
   last_name: 'Attali',
-  description: 'J\'ai seule boussole qui indique toujours le sud',
+  description: 'J\'ai la seule boussole qui indique toujours le sud',
   age: 87,
   remote_avatar_url: 'http://s2.lemde.fr/image/2016/08/26/644x0/4988464_6_e89b_jacques-attali-alors-conseiller-special-de_42e000872eb903b70c800c81e1ede3ae.jpg'
   )
@@ -58,13 +58,13 @@ UniScrapper.new(hike_solutre, "https://www.visorando.com/randonnee-sur-les-pas-d
 # Solutre - Trip creation
 trip_solutre = Trip.create!(
   title: "Ascension de La Roche de Solutré - sur les pas de Tonton",
-  description: "Pélerinage annuel pour rendre hommage aux résitants de Solutré",
-  start_location: "Paris",
+  description: "Pélerinage annuel pour honorer une promesse faite aux anciens résistants de la roche de Solutré après la Seconde Guerre Mondiale. Venez nombreux, tout le monde est bienvenue surtout si vous êtes journalistes, le rythme sera détendu",
+  start_location: "Lyon",
   hike_id: hike_solutre.id,
   user_id: francois.id,
   date: Date.today+(1),
   trip_type: "Détente",
-  seats: 5,
+  seats: 7,
   auto_accept: true,
   fees: (2..7).to_a.sample
 )
@@ -85,7 +85,7 @@ Submission.create!(
   )
 
 Submission.create!(
-    content: "C'est la fete",
+    content: "C'est la fête",
     user_id: jack.id.to_i,
     trip_id: trip_solutre.id.to_i,
     accepted: true
@@ -113,7 +113,7 @@ vincent = User.create!(
   password: 'aaaaaa',
   first_name: 'Vincent',
   last_name: 'Elbaz',
-  description: "C\'est la fete quand meme",
+  description: "La vérité si je mens",
   age: 47,
   remote_avatar_url: 'https://www.notrecinema.com/images/usercontent/star/vincent-elbaz-photo_35923_14761.jpg'
   )
@@ -123,7 +123,7 @@ karine = User.create!(
   password: 'aaaaaa',
   first_name: 'Karine',
   last_name: 'Viard',
-  description: 'Vive la musique en rando!',
+  description: 'J\'adore les béliers',
   age: 52,
   remote_avatar_url: 'https://sacrenathalie.files.wordpress.com/2015/08/randonneurs-1997-15-g.jpg'
   )
@@ -134,11 +134,11 @@ UniScrapper.new(hike_corse, "https://www.visorando.com/randonnee-gr20-de-bibi/")
 # CORSE - Trip creation
 trip_corse = Trip.create!(
   title: "Sentier de Grande randonnée 20",
-  description: "Grosse rando sympa avec Benoit le Belge",
+  description: "Grosse rando sympa dans la bonne humeur. Venez si vous voulez décompresser de la ville, de ses bruits et de ses pigeons.",
   start_location: "Bastia",
   hike_id: hike_corse.id,
   user_id: benoit.id,
-  date: Date.today+(2),
+  date: Date.today+(1),
   trip_type: "Sportive",
   seats: 4,
   auto_accept: false,
@@ -168,7 +168,7 @@ louis = User.create!(
   password: 'aaaaaa',
   first_name: 'Louis',
   last_name: 'Delon',
-  description: 'je suis un passionné de montagne',
+  description: 'Je suis un passionné de montagne',
   age: 40,
   remote_avatar_url: 'http://kitt.lewagon.com/placeholder/users/loulouman34'
   )
@@ -178,17 +178,17 @@ etienne = User.create!(
   password: 'aaaaaa',
   first_name: 'Etienne',
   last_name: 'Delorieux',
-  description: 'je suis un passionné de rando',
+  description: 'Je suis un passionné de rando',
   age: 27,
   remote_avatar_url: 'https://kitt.lewagon.com/placeholder/users/EtienneDelorieux'
 )
 
 alex = User.create!(
-  email: 'alex@gmail.com',
+  email: 'alexandre_bouvier@hotmail.com',
   password: 'aaaaaa',
   first_name: 'Alexandre',
   last_name: 'Bertrand',
-  description: 'je suis un vrai pro de la rando',
+  description: 'Je suis un vrai pro de la rando',
   age: 32,
   remote_avatar_url: 'https://kitt.lewagon.com/placeholder/users/alexandrebk'
 )
@@ -214,20 +214,20 @@ MultiScrapper.departments_list
 
 # TRIPS
 puts "Creating trips..."
-TRIP_TYPES = ["Sportive", "Détente", "Photo", "Amicale"]
+TRIP_TYPES = ["Sportive", "Détente", "Loisir", "Amicale"]
 
 #creation of a trip by Louis
 hike_louis = (Hike.first.id..Hike.last.id).to_a.sample
 trip = Trip.create!(
-  description: "j'organise une rando dans le vercors",
+  description: "J'organise une rando dans le Vercors",
   start_location: "Lyon",
   hike_id: hike_louis,
-  title: Hike.find(hike_louis).title,
+  title: "Rando dans le Vercors",
   user_id: louis.id,
   date: Date.today+(1),
   trip_type: TRIP_TYPES.sample,
   seats: (2..6).to_a.sample,
-  auto_accept: true,
+  auto_accept: false,
   fees: (2..7).to_a.sample
 )
 
@@ -246,7 +246,7 @@ trip1 = Trip.create!(
 
 )
 
-trip1 = Trip.create!(
+trip2 = Trip.create!(
   title: "Rando dans les Pyrénées et le Pays Basque",
   description: "J'organise une rando au départ de Bayonne mardi prochain. Je ramène du jambon de Bayonne pour le pic-nic :). J'ai un combi Volkswagen",
   start_location: "Bayonne",
@@ -282,14 +282,14 @@ puts "Creating submissions..."
 
 #etienne make a submission request to louis which is accepted
 Submission.create!(
-    content: "je suis tres interessé par votre randonnée",
+    content: "Je suis tres interessé par votre randonnée",
     user_id: etienne.id.to_i,
     trip_id: trip.id.to_i,
     accepted: true
   )
 
 Submission.create!(
-    content: "reste t-il des places?",
+    content: "Reste t-il des places?",
     user_id: alex.id.to_i,
     trip_id: trip.id.to_i,
     accepted: true
@@ -320,7 +320,7 @@ puts "Creating reviews..."
 
 #Etienne add a review to louis
 Review.create!(
-    content: "mec super cool, super balade",
+    content: "Mec super cool, super balade",
     rating: 5,
     sender_id: etienne.id.to_i,
     receiver_id: louis.id.to_i,
@@ -328,11 +328,27 @@ Review.create!(
   )
 
 Review.create!(
-    content: "j'ai adoré, merci!",
+    content: "J'ai adoré, merci!",
     rating: 4,
     sender_id: alex.id.to_i,
     receiver_id: louis.id.to_i,
     trip_id: trip.id.to_i
+  )
+
+Review.create!(
+    content: "Homme très érudit, pas avare d'anedoctes sur ses amis comme ses ennemis.",
+    rating: 5,
+    sender_id: jack.id.to_i,
+    receiver_id: francois.id.to_i,
+    trip_id: trip_corse.id.to_i
+  )
+
+Review.create!(
+    content: "Bon vivant et très très marrant malgré un fort accent belge",
+    rating: 5,
+    sender_id: vincent.id.to_i,
+    receiver_id: benoit.id.to_i,
+    trip_id: trip_corse.id.to_i
   )
 
 10.times do
@@ -351,13 +367,13 @@ end
 puts "Creating messages..."
 
 Message.create!(
-    content: "bonjour je suis super content d'avoir été accepté à votre rando",
+    content: "Bonjour je suis super content d'avoir été accepté à votre rando",
     user_id: etienne.id,
     trip_id: trip.id
   )
 
 Message.create!(
-    content: "bonjour à quelle heure se fait le depart",
+    content: "Bonjour à quelle heure se fait le depart",
     user_id: alex.id,
     trip_id: trip.id
   )
