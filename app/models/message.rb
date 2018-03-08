db9ba9304cf454105907d102e9cbf093ad5861e2
theolal <34456156+theolal@users.noreply.github.com>
2018-03-08 14:13:50 +0100

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :trip
   belongs_to :user
 
-  validates :content, length: { minimum: 20 }
+  validates :content, length: { minimum: 2 }
   validates :user, presence: true
   validates :trip, presence: true
 end
