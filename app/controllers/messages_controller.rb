687f3963bbba20f0830ec6d37ed0e9b95497a35a
alexandrebk <alexandre.b2506@gmail.com>
2018-05-15 15:40:08 +0200

    boucle dans controller message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,19 +8,30 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
-    @trip = Trip.find(params[:trip_id])
-    @message.trip = @trip
-    @message.user = current_user
+    puts "-----"
+    puts "Que passa??? nada"
+    @message       = Message.new(message_params)
+    @trip          = Trip.find(params[:trip_id])
+    @message.trip  = @trip
+    @message.user  = current_user
     authorize @message
     @message.save
-    @new_message = Message.new
+    @submissioners = Submission.where(trip_id: params[:trip_id])
+    puts "Print submissioners"
+    p @submissioners
+    # Message aux participants
+    @submissioners.each do |submissioner|
+      UserMailer.new_message(@message, submissioner).deliver_now
+    end
+    # Message à l'organisateur
+    p @trip
+    UserMailer.new_message(@message, @trip).deliver_now
+    @new_message   = Message.new
   end
 
   def page_title
       "Whikend Messages"
   end
-
 
 private
 
@@ -35,3 +46,4 @@ private
   end
 
 end
+
